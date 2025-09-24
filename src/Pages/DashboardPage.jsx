2fec69bf288b102d91a2527f6fa31abd20558999
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-24 20:34:35 +0530

    collect userData from the redux store

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -5,10 +5,15 @@ import { useState } from "react"
 import { ProjectViewerPage } from "./ProjectViewerPage"
 import { RecentProjects } from "@/components/Dashboard/RecentProjects"
 import { QuickActions } from "@/components/Dashboard/QuickActions"
+import { useSelector } from "react-redux"
 
 
 export default function DashboardPage() {
   const [selectedProject, setSelectedProject] = useState(null)
+
+    const userData = useSelector((state) => state.auth.user);
+
+    
 
   if (selectedProject) {
     return (
@@ -18,21 +23,24 @@ export default function DashboardPage() {
     )
   }
 
+
+  // console.log("userData from dashboard", userData);
+
   return (
-    <div className="min-h-screen bg-background">
-      <DashboardHeader />
-      <main className="container mx-auto px-6 py-8 space-y-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2 space-y-8">
+    <div className='min-h-screen bg-background'>
+      <DashboardHeader userData={userData} />
+      <main className='container mx-auto px-6 py-8 space-y-8'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
+          <div className='lg:col-span-2 space-y-8'>
             <DashboardStats />
             <RecentProjects onProjectClick={setSelectedProject} />
           </div>
-          <div className="space-y-8">
+          <div className='space-y-8'>
             <AIPromptGenerator />
             <QuickActions />
           </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
